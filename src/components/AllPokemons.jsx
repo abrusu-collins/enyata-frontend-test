bff32416eed2841430da8b0ff38729ed8c267f37
abrusu-collins <snillochemingson@gmail.com>
2024-05-17 21:48:41 +0000

    page size and pagination working now

--- src/components/AllPokemons.jsx
+++ src/components/AllPokemons.jsx
@@ -4,17 +4,18 @@ import { getAllPokemons } from "../services/pokemons";
 import { IoEyeSharp } from "react-icons/io5";
 import Pagination from "@mui/material/Pagination";
 import { GoChevronDown } from "react-icons/go";
-import Popover from "@mui/material/Popover";
+// import Popover from "@mui/material/Popover";
 function AllPokemons() {
   const paginationSizes = [8, 12, 16, 24];
   const [displayCount, setDisplayCount] = useState(0);
-  const [offset, setOffset] = useState(0);
-  const [pokemonTotalCount, setPokemonTotalCount] = useState(0);
+  // const [offset, setOffset] = useState(0);
+  // const [pokemonTotalCount, setPokemonTotalCount] = useState(0);
   const [pokemonData, setPokemonData] = useState([]);
   const [displayData, setDisplayData] = useState([]);
   const [pokemonIndex, setPokemonIndex] = useState(null);
   const [paginationSizeIndex, setPaginationIndex] = useState(0);
-
+  const [page, setPage] = useState(1);
+  const [showPageSizeController, setShowPageSizeController] = useState(false);
   const fetchAllPokemons = async () => {
     const tempArray = [];
     getAllPokemons().then((res) => {
@@ -41,11 +42,23 @@ function AllPokemons() {
       changeDisplayData(displayCount);
     }
   }, [displayCount]);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  // const [anchorEl, setAnchorEl] = React.useState(null);
+  // const handleClick = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+  const handleChange = (event, value) => {
+    setPage(value);
+    // console.log(value);
   };
-
+  const changePage = (value) => {
+    const tempArray = [...pokemonData];
+    setDisplayData(
+      tempArray.splice(value * displayCount - displayCount, displayCount)
+    );
+  };
+  useEffect(() => {
+    changePage(page);
+  }, [page]);
   return (
     <div className="all_pokemons">
       <div className="navbar">
@@ -80,9 +93,7 @@ function AllPokemons() {
               >
                 <div className="pokemon_img_container">
                   <img
-                    src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${
-                      index + 1
-                    }.svg`}
+                    src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
                     alt=""
                   />
                 </div>
@@ -102,45 +113,64 @@ function AllPokemons() {
       <div className="pagination_and_page_size_selector">
         <div className="pagination">
           <Pagination
-            count={10}
+            count={Math.ceil(500 / displayCount)}
+            onChange={handleChange}
+            page={page}
             shape="rounded"
             sx={{
               "& .MuiPaginationItem-root": {
                 fontFamily: "clashDisplay",
                 color: "black",
                 backgroundColor: "#e1e1e1",
+                padding: "20px",
                 "&:hover": {
                   backgroundColor: "#e1e1e1",
                 },
                 "&.Mui-selected": {
                   backgroundColor: "#e85382",
                 },
+                "&.MuiPaginationItem-ellipsis": {
+                  background: "transparent",
+                },
               },
             }}
           />
         </div>
-        <div className="popover_and_selector">
-          <button className="page_size_selector" onClick={handleClick}>
+        <div
+          className="popover_and_selector"
+          onMouseEnter={() => {
+            setShowPageSizeController(true);
+          }}
+          onMouseLeave={() => {
+            setShowPageSizeController(false);
+          }}
+        >
+          <button className="page_size_selector">
             <div className="number">{paginationSizes[paginationSizeIndex]}</div>
             <GoChevronDown size={20} />
           </button>
-          <div className="sizes">
-            {paginationSizes.map((paginationSize, index) => {
-              return (
-                <a
-                  href=" "
-                  key={`${index + paginationSize}`}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setPaginationIndex(index);
-                    setDisplayCount(paginationSizes[index]);
-                  }}
-                >
-                  {paginationSize}
-                </a>
-              );
-            })}
-          </div>
+          {showPageSizeController && (
+            <div className="sizes">
+              {paginationSizes.map((paginationSize, index) => {
+                return (
+                  paginationSizeIndex !== index && (
+                    <a
+                      href=" "
+                      key={`${index + paginationSize}`}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setPaginationIndex(index);
+                        setDisplayCount(paginationSizes[index]);
+                        setPage(1);
+                      }}
+                    >
+                      {paginationSize}
+                    </a>
+                  )
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
