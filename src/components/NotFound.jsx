3b7706f5dfe64c28101d3e750c6e64da71887715
abrusu-collins <snillochemingson@gmail.com>
2024-05-20 23:07:14 +0000

    404 page done

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import useStore from "../store/store";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  const theme = useStore((state) => state.theme);
+  return (
+    <div className="not_found">
+      <div>
+        <img src="/images/404.jpg" alt="" />
+        <p>
+          Looks like the page you <br /> are looking for does not exist
+        </p>
+
+        <div>
+          <a href="/" style={{ background: theme }}>
+            Go Home
+          </a>
+          <a href="/all-pokemons" style={{ background: theme }}>
+            View All
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
